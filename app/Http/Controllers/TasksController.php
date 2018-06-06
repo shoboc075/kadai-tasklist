db62b37b2233873af8b2f59e0c89dd358a7d44ef
shohei.imamura <shoboc075@gmail.com>
2018-06-06 06:49:30 +0000

    bootstrap design

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -46,8 +46,7 @@ class TasksController extends Controller
     public function store(Request $request)
     {
         $this->validate($request, [
-            'status' => 'required | max:10',
-            ]);
+            'status' => 'required | max:10', ]);
             
        
         $task = new Task;
@@ -99,10 +98,8 @@ class TasksController extends Controller
      
     public function update(Request $request, $id)
     {
-        $this->validate($request, [
-            'status' => 'required | max:10',
-            ]);
-        
+        $this->validate($request, ['status' => 'required | max:10',]);
+    
         $task = Task::find($id);
         $task->status = $request->status;
         $task->content = $request->content;
